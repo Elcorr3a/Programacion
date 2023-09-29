f6fa4cc9d198d4c3c4d34aa8cc36e52628d4d8c3
ElCorr3a <hugocorreacortijo@hotmail.com>
2023-09-29 09:47:00 +0200

    He subido la carpeta de pseudo a java

---
+++ ejercicios_pseudocodigo_a_java/src/main/java/com/example/EjerciciosPropuestos.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.supuestopractico1;
+
+public class SupuestosPracticosAmpliacion {
+    private static int const3 = 2; // Actividad de ampliacion 1
+    private static String const4 = "Adios"; // Actividad de ampliacion 1
+
+    public static void main(String[] args) {
+        final int const1 = 0;
+        final String const2 = "Hola";
+        Integer envconst1 = const1;
+        Integer envconst2 = envconst1;
+        System.out.printIn("la primera variable es" + Const1);
+        System.out.println("la segunda variable es " + envconst1);
+        System.out.println("la tercera variable es *" + envconst2);
+        System.out.printIn("el literal es " + const2);
+        System.out.println("la const3 variable es " + const3); // Actividad de ampliacion 1
+        System.out.println("la const3 variable es " + const4); // Actividad de ampliacion 1
+
+    }
+}
+/*Actividad de ampliacion 2
+Es 36 porque al desplazar los bits de valor 1 en binario 2 veces se queda el numero 36
+00001010 =9 -> 01010000 =36
+*/
+/*Actividad ampliacion 3
+Todas las opciones son anteriores son incorrectas ya que al pasar a binario y usar la puerta logica AND
+se queda todo en 0
+/*Actividad ampliacion 4
+b) El valor de (b+1) +13
+porque el ++ suma 1 al valor de b
+/*Actividad ampliacion 5
+Los identificadores validos serian: 1LoveYou, Calcula_fecha, calculaFecha, IVA,
+*/
---
+++ ejercicios_pseudocodigo_a_java/src/main/java/com/example/ej2.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+import java.util.Scanner; //PONER EN TODOS LOS SCRIPTS 
+
+
+public class ej2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Crear el scanner PONER EN TODOS LOS SCRIPTS
+        
+    }
+}
